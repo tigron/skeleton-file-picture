a80e52a60e99f3328da53a3519dcc0db42e73613
Lionel Laffineur <lionel@tigron.be>
2018-07-16 11:23:08 +0200

    Created initial migration creating the table

---
+++ migration/20160504_142001_init.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Database migration class
+ *
+ * @author Lionel Laffineur <lionel@tigron.be>
+ */
+namespace Skeleton\Transaction;
+
+use \Skeleton\Database\Database;
+
+class Migration_20160504_142001_Init extends \Skeleton\Database\Migration {
+
+	/**
+	 * Migrate up
+	 *
+	 * @access public
+	 */
+	public function up() {
+		$db = Database::get();
+		$db->query("CREATE TABLE IF NOT EXISTS `picture` (
+					  `id` int(11) NOT NULL AUTO_INCREMENT,
+					  `file_id` int(11) NOT NULL,
+					  `width` int(11) NOT NULL,
+					  `height` int(11) NOT NULL,
+					  PRIMARY KEY (`id`),
+					  KEY `file_id` (`file_id`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+	}
+
+	/**
+	 * Migrate down
+	 *
+	 * @access public
+	 */
+	public function down() {
+
+	}
+}
