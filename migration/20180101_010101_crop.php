838a26237750f09ed338786da31880352711334c
Lionel Laffineur <lionel@tigron.be>
2018-07-16 11:52:55 +0200

    Migration to add columns for crop feature

---
+++ migration/20180101_010101_crop.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Database migration class
+ *
+ * @author Lionel Laffineur <lionel@tigron.be>
+ */
+namespace Skeleton\File\Picture;
+
+use \Skeleton\Database\Database;
+
+class Migration_20180101_010101_Crop extends \Skeleton\Database\Migration {
+
+	/**
+	 * Migrate up
+	 *
+	 * @access public
+	 */
+	public function up() {
+		$db = Database::get();
+
+		// check if the columns for crop functionality are present
+		$columns = $db->get_all("SHOW COLUMNS FROM `picture`;");
+		foreach ($columns as $column) {
+			if ($column['field'] == 'crop_width' || $column['field'] == 'crop_height') {
+				return;
+			}
+		}
+
+		// add the columns needed for crop feature
+		$db->query("ALTER TABLE `picture`
+					ADD `crop_width` INT(11) NOT NULL,
+					ADD `crop_height` INT(11) NOT NULL,
+					ADD `crop_offset_left` INT(11) NOT NULL,
+					ADD `crop_offset_right` INT(11) NOT NULL");
+	}
+
+	/**
+	 * Migrate down
+	 *
+	 * @access public
+	 */
+	public function down() {
+
+	}
+}
